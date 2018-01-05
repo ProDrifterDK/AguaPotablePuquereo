a83bec3fcaf38a9e16854a54f71233cfe8be50c6
vichoste <thunderwearsvicho37@protonmail.com>
2018-01-05 17:15:28 -0300

    View de Error implementado y algunas identaciones porque sí :P
    ayy lmao, TABS ftw

--- AguaPotablePuquereo/Controllers/SessionController.cs
+++ AguaPotablePuquereo/Controllers/SessionController.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace AguaPotablePuquereo.Controllers
-{
-    public class SessionController : Controller
-    {
-        // GET: Session
-        public ActionResult VistaDeudas()
-        {
-            return View();
-        }
+namespace AguaPotablePuquereo.Controllers {
+	public class SessionController : Controller {
+		// GET: Session
+		public ActionResult VistaDeudas() {
+			return View();
+		}
 
-        public ActionResult Deudas(string rut)
-        {
-            return View();
-        }
-    }
+		public ActionResult Deudas(string rut) {
+			return View();
+		}
+		public ActionResult Error() {
+			return View();
+		}
+	}
 }
