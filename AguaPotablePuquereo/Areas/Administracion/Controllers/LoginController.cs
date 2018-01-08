49d9446776feaa2578db9eabbda08a4ac38da150
Daniel Barria <daniel.b.a@hotmail.es>
2018-01-08 18:35:50 -0300

    area administracion login controller (no funcional)

---
+++ AguaPotablePuquereo/Areas/Administracion/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AguaPotablePuquereo.Areas.Administracion.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Administracion/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
