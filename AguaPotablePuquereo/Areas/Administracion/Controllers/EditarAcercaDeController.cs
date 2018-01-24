14af1588b68d1839f3252ecff0083f7d89f9f35a
vichoste <thunderwearsvicho37@protonmail.com>
2018-01-24 16:01:56 -0300

    Cambios (Muchos)
    Cambios en los botones de la navbar en la sección de Admin.
    Cambio en el diseño en la mayoría de los botones.
    Cambio en el tipo de tabla en las deudas en la sección de mantención de cliente.
    Ahora se ven las deudas en la Mantención de cliente, debido a que se sacó una línea del script que correspondía al checkbox.

---
+++ AguaPotablePuquereo/Areas/Administracion/Controllers/EditarAcercaDeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AguaPotablePuquereo.Areas.Administracion.Controllers
+{
+    public class EditarAcercaDeController : Controller
+    {
+        // GET: Administracion/EditarAcercaDe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
