6ee10f54b009567524fe0fd1972b6cbd4aa5ba56
Daniel Barria <daniel.b.a@hotmail.es>
2018-02-13 16:11:27 -0300

    modal para ver si está seguro de su sexualidad

--- AguaPotablePuquereo/Areas/Administracion/Controllers/AdministradorController.cs
+++ AguaPotablePuquereo/Areas/Administracion/Controllers/AdministradorController.cs
@@ -192,6 +192,7 @@ namespace AguaPotablePuquereo.Areas.Administracion.Controllers
                     }
                 }
 
+                BDD.Database.ExecuteSqlCommand("DELETE FROM TBL_PAGOS WHERE CLI_ID = " + id);
                 BDD.Database.ExecuteSqlCommand("DELETE FROM TBL_DEUDA WHERE CLI_ID = " + id);
 
                 BDD.TBL_CLIENTE.Remove(cliente);
